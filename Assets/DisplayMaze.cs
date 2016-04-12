49f1820f0f3ac6d3c9fb675e3e448624532799aa
Mike Stout <stout@uselessopinions.com>
2016-04-11 17:23:50 -0700

    Added a display mode for "bricks" -- which displays a bunch of colored quads to outline the maze

--- Assets/DisplayMaze.cs
+++ Assets/DisplayMaze.cs
@@ -5,6 +5,8 @@ namespace Useless.MazeMaker
 {
     public class DisplayMaze : MonoBehaviour
     {
+        public enum MazeDisplayType { Bricks, Tiles, OutlinerDebug, ConsoleDebug } //Tiles and bricks TBD
+        public MazeDisplayType displayType = MazeDisplayType.OutlinerDebug;
         public Maze maze = null;
         public int _seed = -1;
 
@@ -14,8 +16,8 @@ namespace Useless.MazeMaker
         public int Width = 5;
         public int Height = 5;
 
-        public bool _DebugDraw = true;
-        public bool _DebugPrint = false;
+        //public bool _DebugDraw = true;
+        //public bool _DebugPrint = false;
 
         public void Start()
         {
@@ -27,16 +29,121 @@ namespace Useless.MazeMaker
             maze.Make(_seed);
             maze.Sparsify(sparsenessPasses);
             maze.Loopify(loopinessFactor);
-           
 
-            if (_DebugPrint)
-                print(maze.ToString());
+            switch (displayType)
+            {
+                case MazeDisplayType.OutlinerDebug:
+                    break;
+                case MazeDisplayType.Bricks:
+                    CreateBricks();
+                    break;
+                case MazeDisplayType.Tiles:
+                    break;
+                case MazeDisplayType.ConsoleDebug:
+                    print(maze.ToString());
+                    break;
+            }//switch
         }//Init
+
+        private Texturizer t = null;
+        public void CreateBricks()
+        {
+            GameObject _brickPrefab = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //_brickPrefab.transform.localScale = transform.localScale;
+            Destroy(_brickPrefab.GetComponent<BoxCollider>());
+
+            if (t == null)
+                t = new Texturizer(Color.white);
+
+            int scaleX = 2;
+            int scaleY = 2;
+
+            int xMin = 1 - scaleX;
+            int xMax = scaleX - 1;
+
+            int yMin = 1 - scaleY;
+            int yMax = scaleY - 1;
+
+            int xOffset = scaleX + (scaleX - 1);
+            int yOffset = scaleY + (scaleY - 1);
+            //PlaceAllDoors(scaleX, scaleY);
+            for (int y = 0; y < maze.nodes.GetLength(1); y++)
+            {
+                for (int x = 0; x < maze.nodes.GetLength(0); x++)
+                {
+                    UPoint startingOffset = new Vector3(x * xOffset, y * yOffset);
+                    
+                    for (int yOff = yMin; yOff <= yMax; yOff++)
+                    {
+                        for (int xOff = xMin; xOff <= xMax; xOff++)
+                        {
+                            int links = maze.nodes[x, y].links;
+                            int newLink = 15;
+
+                            if ((yOff == yMin && !maze.isFlagged(links, Maze.DOWN)) ||
+                               (yOff == yMax && !maze.isFlagged(links, Maze.UP)) ||
+                               (xOff == xMax && !maze.isFlagged(links, Maze.RIGHT)) ||
+                               (xOff == xMin && !maze.isFlagged(links, Maze.LEFT)))
+                            {
+                                newLink = 0;
+                            }//if
+                            else if (maze.numLinks(links) < 4 && (
+                                    (yOff == yMin && xOff == xMin) ||
+                                    (yOff == yMax && xOff == xMin) ||
+                                    (yOff == yMin && xOff == xMax) ||
+                                    (yOff == yMax && xOff == xMax)))
+                            {
+                                newLink = 0;
+                            }
+                            else if (yOff == 0 && xOff == 0 && links == 0)
+                            {
+                                newLink = 0;
+                            }//else if
+
+                            UPoint placeAt = startingOffset + new Vector3(xOff, yOff);
+
+                            //UPoint tilesIndex = startingOffset + new UPoint(scaleX - 1, scaleY - 1) + new UPoint(xOff, yOff);
+
+                            t.setupTile(newLink);
+
+
+                            GameObject tile = t.makeObject();
+                            //tile.Initialize(tilesIndex.ix, tilesIndex.iy);
+                            //tile.links = newLink;
+                            tile.transform.position = placeAt;
+                            tile.transform.position += new Vector3(0, 0, 10);
+
+                            tile.transform.parent = transform;
+                            tile.name = "Tile [" + x + "," + y + "]";
+
+                            //if (x == 0 && y == 0 && maze.numLinks(links) == 1)
+                            //{
+                                //tile.tag = "ChestTile";
+                            //}//if
+
+                            //tiles[tilesIndex.ix, tilesIndex.iy] = tile;
+                        }//for
+                    }//for
+                }//for
+            }//for
+
+            _brickPrefab.SetActive(false);
+        }//CreateBricks
 
         public void Update()
         {
-            if (_DebugDraw)
-                DrawDebugLines(transform.position);
+            switch(displayType)
+            {
+                case MazeDisplayType.OutlinerDebug:
+                    DrawDebugLines(transform.position);
+                    break;
+                case MazeDisplayType.Bricks:
+                    break;
+                case MazeDisplayType.Tiles:
+                    break;
+                case MazeDisplayType.ConsoleDebug:
+                    break;
+            }//switch
         }//Update
 
         public void DrawDebugLines(UPoint offset)
